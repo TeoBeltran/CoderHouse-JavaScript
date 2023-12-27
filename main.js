91c2caaba38fc5c8d75724e2bd8bb6a2d2120ffe
Teo Beltrán <109324334+TeoBeltran@users.noreply.github.com>
2023-12-26 23:10:44 -0300

    Segundo Commit

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ function registrarUsuario() {
 
     let nombre = prompt("Ingrese su nombre:");
     let apellido = prompt("Ingrese su apellido:");
-    let genero = prompt("Ingrese su género:");
     let edad = parseInt(prompt("Ingrese su edad:"));
 
-    if (nombre && apellido && genero && !isNaN(edad) && edad > 0) {
-        console.log("Registro exitoso:");
-        console.log("Nombre:", nombre);
-        console.log("Apellido:", apellido);
-        console.log("Género:", genero);
-        console.log("Edad:", edad);
-
-        let contador = 0;
-        while (contador < 3) {
-            console.log("¡Hola", nombre, "!");
-            contador++;
+    let contador = 0;
+    while (contador < 2) {
+        if(contador == 1) {
+            alert("¡Hola otra vez, " + nombre + "!");
         }
+        else{
+            alert("¡Hola " + nombre + "!");
+        }
+        contador++;
+    }
+
+    if (edad > 0) {
+        console.log("Prueba de consol.log correcta!");
 
         alert("¡Registro exitoso!");
     } else {
