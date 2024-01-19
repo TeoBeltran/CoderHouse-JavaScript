05788bf06e75cfe5da57b06085e6839159bde6a1
Teo Beltran <teo.belnan@gmail.com>
2024-01-18 23:01:35 -0300

    Commit 2

--- main.js
+++ main.js
@@ -25,4 +25,46 @@ function registrarUsuario() {
     }
 }
 
-registrarUsuario();
+//registrarUsuario();
+
+
+function Producto(nombre, desc, precio) {
+    this.nombre = nombre;
+    this.desc = desc;
+    this.precio = precio;
+}
+
+var Producto1 = new Producto("Producto1", "Descripción del Producto1", 49.99);
+
+// Actualizar la card del Producto1 en el HTML
+var productoCard1 = document.getElementById("productoCard1");
+
+// Crear elementos HTML con los datos del Producto1
+var card = document.createElement("div");
+card.className = "card";
+
+var titulo = document.createElement("h2");
+titulo.textContent = Producto1.nombre;
+
+var blackBox = document.createElement("div");
+blackBox.className = "black-box";
+
+var desc = document.createElement("p");
+desc.textContent = Producto1.desc;
+
+var button = document.createElement("button");
+button.textContent = "Comprar";
+
+// Agregar elementos al contenedor de la card
+card.appendChild(titulo);
+card.appendChild(blackBox);
+card.appendChild(desc);
+card.appendChild(button);
+
+// Agregar la card al contenedor en el HTML
+productoCard1.appendChild(card);
+
+
+console.log("Nombre del producto: " + Producto1.nombre);
+console.log("Descripción del producto: " + Producto1.desc);
+console.log("Precio del producto: $" + Producto1.precio);
